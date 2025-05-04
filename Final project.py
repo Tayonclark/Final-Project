d83196264d695eb6b9b63e8892ab1021986009a9
RTC-13 <110063185+RTC-13@users.noreply.github.com>
2025-05-04 16:37:11 -0400

    Update Final project.py

--- Final project.py
+++ Final project.py
@@ -8,3 +8,24 @@ def Position_Of_Letters(word):
         else:
             positions.append((letter, 'Not a letter'))
     return positions
+
+
+def score(word):
+    position_list = Position_Of_Letters(word)
+    final_score = 0
+
+    for letter, position in position_list:
+        # checks if position is an intiger (some items in position are "Not a letter")
+        if isinstance(position, int):
+            final_score += position
+    return final_score
+
+
+if __name__ == "__main__":
+    user_input = input("Enter a word or phrase to see total score: ")
+    
+    total_score = score(user_input)
+    print( f'Total score of input: {total_score}')
+
+
+
